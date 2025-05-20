721b95494101d3449186daa17db225a7c322a029
rafaelmendoncadev <recargaoriginal@hotmail.com>
2025-05-20 11:10:05 -0300

    Melhorias no Front End

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             txtTitulo = new TextBox();
             btnAdicionar = new Button();
             btnEditar = new Button();
@@ -35,11 +36,17 @@
             listBoxTarefas = new ListBox();
             txtDescricao = new TextBox();
             btnConcluir = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // txtTitulo
             // 
-            txtTitulo.Location = new Point(21, 12);
+            txtTitulo.BackColor = SystemColors.Info;
+            txtTitulo.Location = new Point(46, 91);
             txtTitulo.Name = "txtTitulo";
             txtTitulo.PlaceholderText = "Título da Tarefa";
             txtTitulo.Size = new Size(306, 23);
@@ -47,43 +54,48 @@
             // 
             // btnAdicionar
             // 
-            btnAdicionar.Location = new Point(19, 91);
+            btnAdicionar.BackColor = SystemColors.Info;
+            btnAdicionar.Location = new Point(41, 383);
             btnAdicionar.Name = "btnAdicionar";
-            btnAdicionar.Size = new Size(68, 23);
+            btnAdicionar.Size = new Size(72, 23);
             btnAdicionar.TabIndex = 1;
             btnAdicionar.Text = "Adicionar";
-            btnAdicionar.UseVisualStyleBackColor = true;
+            btnAdicionar.UseVisualStyleBackColor = false;
             // 
             // btnEditar
             // 
-            btnEditar.Location = new Point(100, 91);
+            btnEditar.BackColor = SystemColors.Info;
+            btnEditar.Location = new Point(122, 383);
             btnEditar.Name = "btnEditar";
-            btnEditar.Size = new Size(68, 23);
+            btnEditar.Size = new Size(72, 23);
             btnEditar.TabIndex = 2;
             btnEditar.Text = "Editar";
-            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.UseVisualStyleBackColor = false;
             // 
             // btnExcluir
             // 
-            btnExcluir.Location = new Point(181, 91);
+            btnExcluir.BackColor = SystemColors.Info;
+            btnExcluir.Location = new Point(203, 383);
             btnExcluir.Name = "btnExcluir";
-            btnExcluir.Size = new Size(68, 23);
+            btnExcluir.Size = new Size(72, 23);
             btnExcluir.TabIndex = 3;
             btnExcluir.Text = "Excluir";
-            btnExcluir.UseVisualStyleBackColor = true;
+            btnExcluir.UseVisualStyleBackColor = false;
             // 
             // listBoxTarefas
             // 
+            listBoxTarefas.BackColor = SystemColors.Info;
             listBoxTarefas.FormattingEnabled = true;
             listBoxTarefas.ItemHeight = 15;
-            listBoxTarefas.Location = new Point(21, 140);
+            listBoxTarefas.Location = new Point(46, 183);
             listBoxTarefas.Name = "listBoxTarefas";
             listBoxTarefas.Size = new Size(306, 184);
             listBoxTarefas.TabIndex = 4;
             // 
             // txtDescricao
             // 
-            txtDescricao.Location = new Point(21, 50);
+            txtDescricao.BackColor = SystemColors.Info;
+            txtDescricao.Location = new Point(46, 142);
             txtDescricao.Name = "txtDescricao";
             txtDescricao.PlaceholderText = "Descrição da Tarefa";
             txtDescricao.Size = new Size(306, 23);
@@ -91,18 +103,64 @@
             // 
             // btnConcluir
             // 
-            btnConcluir.Location = new Point(262, 91);
+            btnConcluir.BackColor = SystemColors.Info;
+            btnConcluir.Location = new Point(284, 383);
             btnConcluir.Name = "btnConcluir";
-            btnConcluir.Size = new Size(68, 23);
+            btnConcluir.Size = new Size(72, 23);
             btnConcluir.TabIndex = 6;
             btnConcluir.Text = "Concluir";
-            btnConcluir.UseVisualStyleBackColor = true;
+            btnConcluir.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 22F, FontStyle.Bold);
+            label1.Location = new Point(249, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(291, 41);
+            label1.TabIndex = 7;
+            label1.Text = "Controle de Tarefas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(46, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Título";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label3.Location = new Point(46, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Descrição";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Gerenciador_de_Tarefa;
+            pictureBox1.Location = new Point(444, 91);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(300, 300);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.MediumAquamarine;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(btnConcluir);
             Controls.Add(txtDescricao);
             Controls.Add(listBoxTarefas);
@@ -110,8 +168,13 @@
             Controls.Add(btnEditar);
             Controls.Add(btnAdicionar);
             Controls.Add(txtTitulo);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "Form1";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Controle de Tarefas";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -125,5 +188,9 @@
         private ListBox listBoxTarefas;
         private TextBox txtDescricao;
         private Button btnConcluir;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox1;
     }
 }
